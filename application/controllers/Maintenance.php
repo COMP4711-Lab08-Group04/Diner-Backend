0b1bc334ddac6148ff05cc911e46c4b3bf9b7ee0
Jason Lee <jasonlee1054@gmail.com>
2017-04-05 11:22:30 -0700

    Setup Maintenance REST controller

---
+++ application/controllers/Maintenance.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * REST server for ferry schedule operations.
+ * This one manages ports.
+ *
+ * ------------------------------------------------------------------------
+ */
+require APPPATH . '/third_party/restful/libraries/Rest_controller.php';
+
+class Maintenance extends Rest_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('menu');
+	}
+
+	function index_get()
+	{
+	    $key = $this->get('id');
+	    if (!$key)
+	    {
+	        $this->response($this->menu->all(), 200);
+	    } else
+	    {
+	        $result = $this->menu->get($key);
+	        if ($result != null)
+	            $this->response($result, 200);
+	        else
+	            $this->response(array('error' => 'Menu item not found!'), 404);
+	    }
+	}
+
+	// Handle an incoming PUT - crUd
+	function index_put()
+	{
+	    $this->response('ok', 200);
+	}
+
+	// Handle an incoming POST - Crud
+	function index_post()
+	{
+	    $this->response('ok', 200);
+	}
+
+	// Handle an incoming DELETE - cruD
+	function index_delete()
+	{
+	    $this->response('ok', 200);
+	}
+
+	// Handle an incoming GET ... return 1 menu item
+	function item_get()
+	{
+	    $key = $this->get('id');
+	    $result = $this->menu->get($key);
+	    if ($result != null)
+	        $this->response($result, 200);
+	    else
+	        $this->response(array('error' => 'Menu item not found!'), 404);        
+	}
+}
